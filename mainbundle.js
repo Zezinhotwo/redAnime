06b5610ba0a23088ced2e0bd411191fc961ba9b3
Zezinhodois <123456789+kaio.bg12@gmail.com>
2024-09-14 20:07:44 -0300

    finaliza para adicao de animacoes

--- mainbundle.js
+++ mainbundle.js
@@ -20,7 +20,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, `*{box-sizing:border-box;margin:0;padding:0}body{background-color:#212121;height:1200px;scroll-behavior:smooth}.menu-toggle{display:none}.menu-icon{display:none;margin-top:-10px}.menu-icon span{display:block;margin:.5em 0;width:40px;height:5px;background-color:#fff;transition:.5s;border-radius:5px}header{background-color:#212121;height:80px}@media screen and (max-width: 468px){.menu-icon{display:block}.menu-toggle:checked+.menu-icon span:first-child{background-color:red;transform:rotate(45deg) translate(9px, 10px);width:40px;border-radius:5px}.menu-toggle:checked+.menu-icon span:nth-child(2){opacity:0%}.menu-toggle:checked+.menu-icon span:last-child{background-color:red;transform:rotate(-45deg) translate(8px, -10px);width:40px;border-radius:5px}header{padding:0 1em;transition:.5s;display:flex;align-items:center;flex-direction:column;z-index:100}.logo{width:30%;margin-top:-10px}nav{width:100%;z-index:-999;opacity:0%;transition:.5s}.plusHome{display:flex;align-items:start;flex-direction:column;height:300px}.hamburg-logo{top:0%;display:flex;align-items:center;justify-content:space-between;width:100%}.hide-show{width:100%;z-index:999;opacity:100%}ul{margin:0;padding:0;list-style:none;margin:.5em 0}ul li{border-radius:10px;padding:.5em;color:#fff}ul li:hover{background:#505050}.slid1{width:100%}.sinopse{margin-top:-1em;background:rgba(16,16,18,.377);background:linear-gradient(355deg, rgba(16, 16, 18, 0.897) 0%, rgb(0, 0, 0) 100%);height:250px;width:100%;padding:1em;color:#fff;display:flex;align-items:start;justify-content:center;flex-direction:column;justify-content:space-between}.sectionSlides{color:#fff;padding:1em}.slides{margin:.5em 0;display:flex;align-items:center;justify-content:center;justify-content:space-around}.animeSlide{width:45%}}/*# sourceMappingURL=style.css.map */`, "",{"version":3,"sources":["webpack://./src/assets/styles/_resets.scss","webpack://./src/assets/styles/style.scss"],"names":[],"mappings":"AAAA,EACI,qBAAA,CACA,QAAA,CACA,SAAA,CAAA,KCCA,wBAAA,CACA,aAAA,CACA,sBAAA,CAGJ,aACI,YAAA,CAEJ,WACI,YAAA,CACA,gBAAA,CAEJ,gBACI,aAAA,CACA,aAAA,CACA,UAAA,CACA,UAAA,CACA,qBAAA,CACA,cAAA,CACA,iBAAA,CAEJ,OACI,wBAAA,CACA,WAAA,CAEJ,qCACI,WACI,aAAA,CAEJ,iDACI,oBAAA,CACA,4CAAA,CACA,UAAA,CACA,iBAAA,CAEJ,kDACI,UAAA,CAEJ,gDACI,oBAAA,CACA,8CAAA,CACA,UAAA,CACA,iBAAA,CAEJ,OACI,aAAA,CACA,cAAA,CACA,YAAA,CACA,kBAAA,CACA,qBAAA,CACA,WAAA,CAEJ,MACI,SAAA,CACA,gBAAA,CAEJ,IACI,UAAA,CACA,YAAA,CACA,UAAA,CACA,cAAA,CAEJ,UACI,YAAA,CACA,iBAAA,CACA,qBAAA,CAEA,YAAA,CAEJ,cACI,MAAA,CACA,YAAA,CACA,kBAAA,CACA,6BAAA,CACA,UAAA,CAEJ,WACI,UAAA,CACA,WAAA,CACA,YAAA,CAEJ,GD/EA,QAAA,CACA,SAAA,CAIA,eAAA,CC4EI,aAAA,CACA,MACI,kBAAA,CACA,YAAA,CACA,UAAA,CAEJ,YACI,kBAAA,CAGR,OACI,UAAA,CAEJ,SACI,eAAA,CACA,8BAAA,CACA,iFAAA,CAEA,YAAA,CACA,UAAA,CACA,WAAA,CACA,UAAA,CACA,YAAA,CACA,iBAAA,CACA,sBAAA,CACA,qBAAA,CACA,6BAAA,CAEJ,eACI,UAAA,CACA,WAAA,CAEJ,QACI,aAAA,CACA,YAAA,CACA,kBAAA,CACA,sBAAA,CACA,4BAAA,CAEJ,YACI,SAAA,CAAA,CAAA,oCAAA","sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, `*{box-sizing:border-box;margin:0;padding:0}body{background-color:#212121;height:1200px;scroll-behavior:smooth}.menu-toggle{display:none}.menu-icon{display:none;margin-top:-10px}.menu-icon span{display:block;margin:.5em 0;width:40px;height:5px;background-color:#fff;transition:.5s;border-radius:5px}header{background-color:#212121;height:80px}@media screen and (max-width: 468px){.menu-icon{display:block}.menu-toggle:checked+.menu-icon span:first-child{background-color:red;transform:rotate(45deg) translate(9px, 10px);width:40px;border-radius:5px}.menu-toggle:checked+.menu-icon span:nth-child(2){opacity:0%}.menu-toggle:checked+.menu-icon span:last-child{background-color:red;transform:rotate(-45deg) translate(8px, -10px);width:40px;border-radius:5px}header{padding:0 1em;transition:.5s;display:flex;align-items:center;flex-direction:column;z-index:100}.logo{width:30%;margin-top:-10px}nav{width:100%;z-index:-999;opacity:0%;transition:.5s}.plusHome{display:flex;align-items:start;flex-direction:column;height:300px}.hamburg-logo{top:0%;display:flex;align-items:center;justify-content:space-between;width:100%}.hide-show{width:100%;z-index:999;opacity:100%}ul{margin:0;padding:0;list-style:none;margin:.5em 0}ul li{border-radius:10px;padding:.5em;color:#fff}ul li:hover{background:#505050}.slid1{width:100%}.sinopse{margin-top:-1em;background:rgba(16,16,18,.377);background:linear-gradient(355deg, rgba(16, 16, 18, 0.897) 0%, rgb(0, 0, 0) 100%);text-align:justify;height:250px;width:100%;padding:1em;color:#fff;display:flex;align-items:start;justify-content:center;flex-direction:column;justify-content:space-between}.sectionSlides{color:#fff;padding:1em}.slides{margin:.5em 0;display:flex;align-items:center;justify-content:center;justify-content:space-around}.animeSlide{width:45%}.wind{width:100%;padding:1em}.wind__text{padding:1em;color:#fff;height:250px;text-align:justify;display:flex;justify-content:center;flex-direction:column;justify-content:space-around}.kimetsu{width:100%;padding:1em}.kimetsu__text{padding:1em;color:#fff;height:250px;text-align:justify;display:flex;justify-content:center;flex-direction:column;justify-content:space-around}footer{display:flex;align-items:center;justify-content:center}.footer-img{width:50%;position:absolute;left:-20%;z-index:-10}.rodape{padding:1em;display:flex;align-items:center;justify-content:center;flex-direction:column}.rodape ul{font-size:10px;font-family:"Courier New",Courier,monospace;display:flex;align-items:center;justify-content:center;justify-content:space-around;flex-wrap:wrap}.rodape ul li{list-style:circle}}/*# sourceMappingURL=style.css.map */`, "",{"version":3,"sources":["webpack://./src/assets/styles/_resets.scss","webpack://./src/assets/styles/style.scss"],"names":[],"mappings":"AAAA,EACI,qBAAA,CACA,QAAA,CACA,SAAA,CAAA,KCCA,wBAAA,CACA,aAAA,CACA,sBAAA,CAGJ,aACI,YAAA,CAEJ,WACI,YAAA,CACA,gBAAA,CAEJ,gBACI,aAAA,CACA,aAAA,CACA,UAAA,CACA,UAAA,CACA,qBAAA,CACA,cAAA,CACA,iBAAA,CAEJ,OACI,wBAAA,CACA,WAAA,CAEJ,qCACI,WACI,aAAA,CAEJ,iDACI,oBAAA,CACA,4CAAA,CACA,UAAA,CACA,iBAAA,CAEJ,kDACI,UAAA,CAEJ,gDACI,oBAAA,CACA,8CAAA,CACA,UAAA,CACA,iBAAA,CAEJ,OACI,aAAA,CACA,cAAA,CACA,YAAA,CACA,kBAAA,CACA,qBAAA,CACA,WAAA,CAEJ,MACI,SAAA,CACA,gBAAA,CAEJ,IACI,UAAA,CACA,YAAA,CACA,UAAA,CACA,cAAA,CAEJ,UACI,YAAA,CACA,iBAAA,CACA,qBAAA,CAEA,YAAA,CAEJ,cACI,MAAA,CACA,YAAA,CACA,kBAAA,CACA,6BAAA,CACA,UAAA,CAEJ,WACI,UAAA,CACA,WAAA,CACA,YAAA,CAEJ,GD/EA,QAAA,CACA,SAAA,CAIA,eAAA,CC4EI,aAAA,CACA,MACI,kBAAA,CACA,YAAA,CACA,UAAA,CAEJ,YACI,kBAAA,CAGR,OACI,UAAA,CAEJ,SACI,eAAA,CACA,8BAAA,CACA,iFAAA,CACA,kBAAA,CACA,YAAA,CACA,UAAA,CACA,WAAA,CACA,UAAA,CACA,YAAA,CACA,iBAAA,CACA,sBAAA,CACA,qBAAA,CACA,6BAAA,CAEJ,eACI,UAAA,CACA,WAAA,CAEJ,QACI,aAAA,CACA,YAAA,CACA,kBAAA,CACA,sBAAA,CACA,4BAAA,CAEJ,YACI,SAAA,CAGJ,MACI,UAAA,CACA,WAAA,CAEJ,YACI,WAAA,CACA,UAAA,CACA,YAAA,CACA,kBAAA,CACA,YAAA,CACA,sBAAA,CACA,qBAAA,CACA,4BAAA,CAEJ,SACI,UAAA,CACA,WAAA,CAEJ,eACI,WAAA,CACA,UAAA,CACA,YAAA,CACA,kBAAA,CACA,YAAA,CACA,sBAAA,CACA,qBAAA,CACA,4BAAA,CAEJ,OACI,YAAA,CACA,kBAAA,CACA,sBAAA,CAEJ,YACI,SAAA,CACA,iBAAA,CACA,SAAA,CACA,WAAA,CAEJ,QACI,WAAA,CACA,YAAA,CACA,kBAAA,CACA,sBAAA,CACA,qBAAA,CACA,WACI,cAAA,CACA,2CAAA,CACA,YAAA,CACA,kBAAA,CACA,sBAAA,CACA,4BAAA,CACA,cAAA,CACA,cACI,iBAAA,CAAA,CAAA,oCAAA","sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -88,8 +88,10 @@ var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _assets_styles_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../assets/styles/style.css */ "./src/assets/styles/style.css");
 /* harmony import */ var _assets_img_logo_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../assets/img/logo.png */ "./src/assets/img/logo.png");
-/* harmony import */ var prettier__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! prettier */ "./node_modules/prettier/standalone.mjs");
+/* harmony import */ var _assets_img_footer_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../assets/img/footer.png */ "./src/assets/img/footer.png");
+/* harmony import */ var prettier__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! prettier */ "./node_modules/prettier/standalone.mjs");
 // document.body.innerText = "Ola, MUNDO";
+
 
 
 
@@ -97,9 +99,12 @@ __webpack_require__.r(__webpack_exports__);
 const $logo = document.querySelector(".logo");
 $logo.src = _assets_img_logo_png__WEBPACK_IMPORTED_MODULE_1__;
 
+const $footer = document.querySelector(".footer-img");
+$footer.src = _assets_img_footer_png__WEBPACK_IMPORTED_MODULE_2__;
+
 const $slide = document.querySelector(".slid1");
 const slide =
-  "https://www.teahub.io/photos/full/33-339920_tokyo-ghoul-desktop-background.jpg";
+  "https://www.canalterror.com.br/wp-content/uploads/2020/01/tokyoghoul.jpg";
 $slide.src = slide;
 
 const $animeSlide = document.querySelectorAll(".animeSlide");
@@ -107,6 +112,15 @@ const animeSlide =
   "https://i.pinimg.com/564x/e2/37/72/e23772515fabaa62913d4bfc1f524679.jpg";
 $animeSlide.forEach((index) => (index.src = animeSlide));
 
+const $wind = document.querySelector(".wind");
+const wind =
+  "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/c3705f0653418420369d475cbff2e2c8.jpe";
+$wind.src = wind;
+
+const $kimetsu = document.querySelector(".kimetsu");
+const kimetsu =
+  "https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/6b17182a3518d7406f0e69687f773f4f.jpg";
+$kimetsu.src = kimetsu;
 // Hamburg
 
 document.querySelector("#menu-toggle").addEventListener("click", () => {
@@ -126,6 +140,16 @@ window.addEventListener("scroll", () => {
   }
 });
 
+
+/***/ }),
+
+/***/ "./src/assets/img/footer.png":
+/*!***********************************!*\
+  !*** ./src/assets/img/footer.png ***!
+  \***********************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "66a19da0c69e35dcf352.png";
 
 /***/ }),
 
